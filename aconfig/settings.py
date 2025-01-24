5ae5e66375105b27849d2821ed54f4676083ce1a
muk3shh07 <dhamimukesh076@gmail.com>
2025-01-24 14:30:07 +0545

    allowed host updated

--- aconfig/settings.py
+++ aconfig/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["aconfig-123.up.railway.app", "localhost" , "127.0.0.1"]
+ALLOWED_HOSTS = ["saleways-123.up.railway.app", "localhost" , "127.0.0.1"]
 
 
 # Application definition
