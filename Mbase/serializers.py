06f567a3ea2a155fe930afb007e10fe4fe021919
muk3shh07 <dhamimukesh076@gmail.com>
2025-01-24 14:46:19 +0545

    serializers updated

--- Mbase/serializers.py
+++ Mbase/serializers.py
@@ -63,4 +63,124 @@ class UserSerializerWithToken(UserSerializer):
         token = RefreshToken.for_user(obj)
         return str(token.access_token)
 
- 
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ["product", "user", "name", "createdAt", "rating", "comment"]
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+
+class ColorSerializer(serializers.ModelSerializer):
+    product_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Color
+        fields = ("_id", "name", "hex_code", "product_count")
+
+    def get_product_count(self, obj):
+        return obj.product_set.count()
+
+
+class SizeSerializer(serializers.ModelSerializer):
+    product_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Size
+        fields = ("_id", "name", "description", "product_count")
+
+    def get_product_count(self, obj):
+        return obj.product_set.count()
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = "__all__"
+
+
+class DiscountOffersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DiscountOffers
+        fields = "__all__"  # Include all fields
+
+    # def to_representation(self, instance):
+    #     response = super().to_representation(instance)
+    #     response["is_active"] = instance.is_active()  # Add a field for active status
+    #     return response
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    reviews = serializers.SerializerMethodField(read_only=True)
+    colors = ColorSerializer(many=True)
+    categories = CategorySerializer(many=True)
+    size = SizeSerializer(many=True)
+    image_albums = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+    def get_reviews(self, obj):
+        reviews = obj.reviews.all()
+        serializer = ReviewSerializer(reviews, many=True)
+        return serializer.data
+
+    def get_image_albums(self, obj):
+        image_albums = ImageAlbum.objects.filter(product=obj)
+        return ImageAlbumSerializer(image_albums, many=True).data
+
+    # def get_colors(self, obj):
+    #     colors = obj.color_set.all()
+    #     return ColorSerializer(colors, many=True).data
+
+
+class ImageAlbumSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ImageAlbum
+        fields = "__all__"
+
+
+class ShippingAddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ShippingAddress
+        fields = "__all__"
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = "__all__"
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    # custom ones
+    orderItems = serializers.SerializerMethodField(read_only=True)
+    shippingAddress = serializers.SerializerMethodField(read_only=True)
+    user = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Order
+        fields = "__all__"
+
+    def get_orderItems(self, obj):
+        items = obj.orderitem_set.all()
+        serializer = OrderItemSerializer(items, many=True)
+        return serializer.data
+
+    def get_shippingAddress(self, obj):
+        try:
+            address = ShippingAddressSerializer(obj.shippingaddress, many=False).data
+        except:
+            address = False
+        return address
+
+    def get_user(self, obj):
+        user = obj.user
+        serializer = UserSerializer(user, many=False)
+        return serializer.data
